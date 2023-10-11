2efced8cbd0aeb207a94b589f893266be97fc228
Aditi Gemini <84580515+aditi1515@users.noreply.github.com>
2023-10-11 16:26:33 +0000

    container config done

---
+++ 01_ Basics/variables.js
@@ -0,0 +1,13 @@
+const id = 1;
+let name = "Aditi";
+var mail = "adi@123"
+state = "UP";
+
+// id = 2;
+// console.log(id);
+
+name = "Neeti";
+mail = "adfi@1919";
+state = "MP";
+
+console.table([ name , mail , state]);
